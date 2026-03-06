b21035df9f1d4a9e1519395c2ddb80ee6664c169
lynneth tigse pena <lyn@Mac.lan>
2026-03-06 11:30:25 -0600

    adding the base code

---
+++ src/main/java/com/example/FourLoop/FourLoopApplication.java
@@ -0,0 +1,13 @@
+package com.example.FourLoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourLoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourLoopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FourLoop/FourLoopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FourLoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourLoopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
